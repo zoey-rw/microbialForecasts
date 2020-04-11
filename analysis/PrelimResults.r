be75cd4ca0c08bc80f25ce569c196f134b7d76b9
Zoey Werbin <zoeywerbin@gmail.com>
2020-04-10 20:43:27 -0400

    removing hard-coded filepaths

---
+++ analysis/PrelimResults.r
@@ -0,0 +1,28 @@
+---
+title: "PreliminaryResults"
+author: "Zoey Werbin"
+date: "4/10/2020"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = FALSE)
+```
+
+# Results
+
+This analysis explored spatial and temporal trends in the relative abundances of dozens of microbial groups at 5 sites within the United States (Fig 1). Overall, spatial variability eclipsed temporal variability (Fig 2), though many groups displayed sensitivity to environmental conditions (Fig 3). Our predictive ability varied by taxonomic ranking (Fig 4). The majority of our predictive uncertainty came from process error (Fig 5), indicating either that more complexity to capture microbial abundance trends, or that there is inherent stochasticity that statistical models cannot explain.
+
+```{r cars}
+summary(cars)
+```
+
+## Including Plots
+
+You can also embed plots, for example:
+
+```{r pressure, echo=FALSE}
+plot(pressure)
+```
+
+Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
