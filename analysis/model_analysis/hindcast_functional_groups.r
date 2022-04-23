15fd43c87247056dec141a1cec782c585b3fb080
zoey-rw <zoeywerbin@gmail.com>
2022-04-23 16:46:56 -0400

    rewrite functional group forecasts using tweaked taxonomic script - still need to investigate output from expit()

--- analysis/model_analysis/hindcast_functional_groups.r
+++ analysis/model_analysis/hindcast_functional_groups.r
@@ -1,159 +1,179 @@
-# Create forecasts for functional groups
-library(tidyverse)
-model_outputs <- readRDS("/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/data/summary/fg_summaries.rds")
-source("/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/source.R")
-source("/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/functions/prepFunctionalData.r")
-source("/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/functions/forecastFunctional.r")
+
+# Create forecasts for taxonomic groups, using structure from SOBOL code
+source("./source.R")
+source("./functions/prepFunctionalData.r")
+source("./functions/forecastFunctional.r")
+
+pacman::p_load(readxl, rjags, Rfast, moments, scales, data.table, doParallel)
+
+# Set some global parameters
+Nmc_AB <- 5000 # Number of samples for subset
+Nmc <- 5000 
+N.beta = 8 
+k = 10
+
+k = 1
 
 # Read in microbial abundances
-cal <- c(readRDS("/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/data/clean/cal_groupAbundances_16S_2021.rds"), 
-				 readRDS("/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/data/clean/cal_groupAbundances_ITS_2021.rds"))
-val <- c(readRDS("/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/data/clean/val_groupAbundances_16S_2021.rds"), 
-				 readRDS("/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/data/clean/val_groupAbundances_ITS_2021.rds"))
+cal <- c(readRDS("./data/clean/cal_groupAbundances_16S_2021.rds"), 
+				 readRDS("./data/clean/cal_groupAbundances_ITS_2021.rds"))
+val <- c(readRDS("./data/clean/val_groupAbundances_16S_2021.rds"), 
+				 readRDS("./data/clean/val_groupAbundances_ITS_2021.rds"))
 
-# Alternative to parallel
-#output.list <- list()
-#for (rank in ranks.keep){
-	
-library(doParallel)
-cl <- makeCluster(28,  outfile="")
+summaries <- readRDS("./data/summary/fg_summaries.rds")
+
+# Loop through each model
+
+max.date = "20200101"
+
+model_name = "cycl_only"
+#model_name = "all_covariates"
+scenario = "full_uncertainty"
+
+
+cl <- makeCluster(10, type="FORK", outfile="")
 registerDoParallel(cl)
-# Run in parallel 
-# k <- 12
-output.list = foreach(k=1:length(keep_fg_names),
-										 # .export=c("fcast_all_plots","ranks.keep","data_in"), .packages = c("tidyr","tidyverse","nimble"),
-											.errorhandling = 'pass') %dopar% {
-												
-												library(tidyverse)
-												
-												# Only functional groups; subset to one rank.
-												rank.name <- keep_fg_names[k]
-												cal.rank.df <- cal[[rank.name]] 
-												val.rank.df <- val[[rank.name]] 
-												rank.df <- rbind(cal.rank.df, val.rank.df)	
-												rank.df <- rank.df[!rank.df$siteID %in% c("ABBY","LAJA"),]
-												
-												# Model truth/inputs
-												# Prep validation data
-												model.cal.dat <- prepFunctionalData(rank.df = rank.df, 
-																														min.prev = 3, max.date = "20170101")
-												model_val <- prepFunctionalData(rank.df = 
-																													rank.df,#[rank.df$plotID %in% model.cal.dat$plotID,], 
-																												min.prev = 3, max.date = "20200101", full_timeseries = T)
-												
-												
-												scenario_list <- list()
-												for (scenario in c("full_uncertainty")){
-													
-													
-													model_list <- list()
-										
-													for (model_name in c("cycl_only", "all_covariates")){
-													
-												#for (scenario_name in names(model_outputs)){
-												out <- fcast_all_plots(taxon_name = rank.name, 
-																																		 model_val = model_val, 
-																																		 scenario = scenario,
-																				model_outputs = model_outputs,
-																				test=F,
-																				model_name = model_name,
-																				time_period = "calibration",
-																				Nmc = 10000, IC = .01)
-												
-												model_list[[model_name]] <- out
-												
-													}
-													out_scenario <- do.call(rbind, model_list)
-													scenario_list[[scenario]] <- out_scenario
-												}
-												
-												scenarios <- do.call(rbind, scenario_list)
-												return(scenarios)
-											}
 
-out <- do.call(rbind, output.list)
+rank_output_list <- list()
+
+#Run for multiple chains, in parallel (via PSOCK)
+# rank_output_list = foreach(k=c(1:10),
+# 											.errorhandling = 'pass') %do% {
+#for (k in 2:10){
+output.list = foreach(k=1:length(keep_fg_names), 	.errorhandling = 'pass') %do% {
+	
+	rank.name <- keep_fg_names[k]
+	
+	message("Beginning forecast loop for: ", rank.name)
+	
+	cal.rank.df <- cal[[rank.name]] 
+	val.rank.df <- val[[rank.name]] 
+	rank.df <- rbind(cal.rank.df, val.rank.df)	
+	rank.df <- rank.df[!rank.df$siteID %in% c("ABBY","LAJA"),]
+	
+	# Prep validation data
+	model.inputs <- prepFunctionalData(rank.df = rank.df, min.prev = 3, max.date = max.date,	full_timeseries = T)
+	
+	
+	model_output_list <- list()
+	for (model_name in c("all_covariates", "cycl_only")){
+		message("Forecasting with model: ", model_name)
+		
+		# Filter model estimates for each plot abundance
+		plot_summary <- summaries$full_uncertainty$plot_est %>% filter(taxon == rank.name &
+																																	 	model_name == !!model_name &
+																																	 	scenario == !!scenario & 
+																																	 	time_period == "calibration")
+		
+		# Get model outputs
+		f <- file.path("./data/model_outputs/functional_groups/", model_name,  paste0("/calibration_samples_", rank.name, "_full_uncertainty.rds"))
+		read_in <- readRDS(f)
+		param_samples_orig <- read_in$samples
+		model.dat <- read_in$metadata$model_data
+		truth.plot.long <- model.dat
+		plot_site_key <- model.dat %>% select(siteID, plotID, dateID, date_num, plot_num, site_num) %>% distinct()
+		site_list <- unique(plot_site_key$siteID)
+		
+		
+		
+		# Use new model inputs for full date, site, and plot keys
+		date_key <- model.inputs$truth.plot.long %>% select(dateID, date_num) %>% distinct()
+		new_plot_site_key <- model.inputs$truth.plot.long %>% select(siteID, plotID, dateID, date_num, plot_num, site_num) %>% 
+			distinct() %>% filter(!siteID %in% plot_site_key$siteID)
+		new_site_list <- unique(new_plot_site_key$siteID)
+		
+		# Prep MCMC sampling IDs
+		param_samples <- as.data.frame(as.matrix(param_samples_orig))
+		Nmc_large <- max(nrow(param_samples)) #20000 # Larger sample number for covariate/IC set of values
+		row_samples <- sample.int(max(nrow(param_samples)),Nmc_AB)
+		ic = Rfast::Rnorm(Nmc_AB,0,1) # Initial condition uncertainty
+		
+		full_site_list <- c(site_list, new_site_list)
+		site_output_list <- list()
+		
+		siteID <- site_list[[1]] #testing
+		siteID <- new_site_list[[1]] #testing
+		for (siteID in full_site_list[c(1, 21)]){
+			message("SiteID: ", siteID)
+			
+			# Change based on each site
+			start_date <- model.inputs$site_start[siteID]
+			NT = model.inputs$N.date
+			
+			if (siteID %in% new_plot_site_key$siteID) {
+				plot_key <- new_plot_site_key %>% filter(siteID == !!siteID)
+				plot_list <- unique(plot_key$plotID)
+			} else {
+				plot_key <- plot_site_key %>% filter(siteID == !!siteID)
+				plot_list <- unique(plot_key$plotID)
+			}
+			plot_output_list <- list()
+			plotID <- plot_list[[1]]
+			for (plotID in plot_list[1:2]){
+				message("PlotID: ", plotID)
+				
+				#Sample covariate data
+				covar_full <- array(NA, dim = c(Nmc_large, N.beta, NT))
+				set.seed(1)
+				for (time in start_date:NT) {
+					covar_full[,,time] <- c(Rfast::Rnorm(Nmc_large, model.inputs$temp[siteID, time],
+																							 model.inputs$temp_sd[siteID, time]),
+																	Rfast::Rnorm(Nmc_large, model.inputs$mois[siteID, time],
+																							 model.inputs$mois_sd[siteID, time]),
+																	Rfast::Rnorm(Nmc_large, model.inputs$pH[plotID,],
+																							 model.inputs$pH_sd[plotID,]),
+																	Rfast::Rnorm(Nmc_large, model.inputs$pC[plotID,],
+																							 model.inputs$pC_sd[plotID,]),
+																	rep(model.inputs$relEM[plotID, time], Nmc_large),
+																	rep(model.inputs$LAI[siteID, time], Nmc_large),
+																	rep(model.inputs$y_sin[time], Nmc_large),
+																	rep(model.inputs$y_cos[time], Nmc_large))
+				}
+				
+				covar <- covar_full[row_samples,,]
+				#go for it!!!
+				hindcast.plot <- fg_fcast(plotID,
+																	covar,
+																	param_samples,
+																	ic,
+																	truth.plot.long,
+																	Nmc = 5000,  plot_summary,plot_start_date)
+				
+				hindcast.plot$dateID <- date_key[match(hindcast.plot$date_num, date_key$date_num),]$dateID
+				hindcast.plot <- hindcast.plot %>% mutate(dates = fixDate(dateID),
+																									model_name = !!model_name,
+																									time_period = "calibration")
+				plot_output_list[[plotID]] <- hindcast.plot
+				
+			}
+			site_output_list[[siteID]] <- rbindlist(plot_output_list)	
+		}
+		model_output_list[[model_name]] <- rbindlist(site_output_list, fill = T)	
+	}
+	#rank_output_list[[rank.name]] <- rbindlist(model_output_list)	
+	rank_output <- rbindlist(model_output_list)	
+	rank_output_list[[rank.name]] = rank_output
+	return(rank_output)
+}				
+
+out <- rbindlist(rank_output_list)	
+out <- rbindlist(output.list)	
 out$fcast_period <- ifelse(out$dates < "2017-01-01", "calibration", "hindcast")
 saveRDS(out, "/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/data/summary/hindcast_fg.rds")
 
 
-stopCluster(cl)
 
+ggplot(out %>% filter(plotID=="BART_002" & taxon == "oligotroph")) + 
+	facet_grid(#rows=vars(taxon), 
+		cols = vars(model_name), drop=T, scales="free") +
+	geom_line(aes(x = dates, y = mean), show.legend = F, linetype=2) +
+	geom_line(aes(x = dates, y = `50%`), show.legend = F) +
+	geom_ribbon(aes(x = dates, ymin = lo, ymax = hi), alpha=0.6, fill="blue") +
+	geom_ribbon(aes(x = dates, ymin = `2.5%`, ymax = `97.5%`),fill="red", alpha=0.6) +
+	theme_bw()+
+	scale_fill_brewer(palette = "Paired") +
+	theme(text = element_text(size = 14), panel.spacing = unit(.2, "cm"),
+				legend.position = "bottom",legend.title = element_text(NULL),
+				plot.margin = unit(c(.2, .2, 2, .2), "cm")) + ylab(NULL) +
+	geom_point(aes(x = dates, y = as.numeric(truth))) + xlab(NULL) + labs(fill='')
 
-
-out.data_all <- readRDS("/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/data/summary/hindcast_fg.rds")
-#out.data <- scenarios
-
-
-out.data <- out.data_all %>% filter(taxon_name == "copiotroph")
-out.data <- out.data_all %>% filter(taxon_name == "ectomycorrhizal")
-
-#out.data <- out.data[out.data$time_period != "refit",]
-out.data <- out.data[out.data$plotID %in% c("HARV_001","BART_001","DSNY_001"),]
-out.data <- out.data[out.data$plotID %in% c("UKFS_031","NOGP_003","KONZ_024"),]
-out.data <- out.data[out.data$plotID %in% c("BART_001","CPER_002","HARV_004"),]
-
-# Simple CI
-output.plot <- ggplot(out.data) +
-					facet_grid(rows=vars(plotID), drop=T, scales="free") +
-	geom_line(data = out.data[out.data$fcast_period =="calibration",], 
-						aes(x = dates, y = `50%`), show.legend = F) + 
-	geom_line(data = out.data[out.data$fcast_period =="hindcast",], 
-						aes(x = dates, y = med), show.legend = F) + 
-	geom_ribbon(data = out.data[out.data$fcast_period =="calibration",],
-							aes(x = dates, ymin = `2.5%`, ymax = `97.5%`, fill = fcast_period), alpha=0.6,  na.rm = F) +
-	geom_ribbon(data = out.data[out.data$fcast_period =="hindcast",],
-							aes(x = dates, ymin = lo, ymax = hi, fill = fcast_period), alpha=0.6,  na.rm = F) +
-	labs(title = "Example hindcasts (calibration: 2013-2016)") + 
-				theme_bw()+
-				scale_fill_brewer(palette = "Paired") + 
-				theme(text = element_text(size = 14), panel.spacing = unit(.2, "cm"), 
-							legend.position = "bottom",legend.title = element_text(NULL),
-							plot.margin = unit(c(.2, .2, 2, .2), "cm")) + ylab(NULL) + 
-				geom_point(aes(x = dates, y = as.numeric(truth))) + xlab(NULL) + labs(fill='') 
-				# facet_grid_paginate(#cols=vars(taxon), rows=vars(plotID), 
-				# 	plotID~taxon,
-				# 	ncol = 1, nrow = 8, page = i)
-
-output.plot
-
-
-
-ggplot(out.data) +
-	facet_grid(rows=vars(plotID), cols=vars(scenario), drop=T, scales="free", space="free") +
-	geom_line(aes(x = dates, y = `med`), show.legend = F) + 
-	geom_ribbon(aes(x = dates, ymin = `lo`, ymax = `hi`, fill = taxon_name), 
-							fill = "darkblue",
-							alpha=0.4, show.legend = F) +
-	#theme_ipsum(base_size = 18, strip_text_size = 22) + 
-	#	ggtitle(paste0("Functional group hindcasts at 3 new plots")) + #scale_x_date() +	
-	theme_minimal(base_size=20) +
-	#scale_fill_brewer(palette = "Paired") + 
-	theme(panel.spacing = unit(.2, "cm"),
-				plot.margin = unit(c(.2, .2, .2, .2), "cm")) +#+ ylim(c(1.5,7)) +
-	ylab("Abundance") + 
-	geom_point(aes(x = dates, y = as.numeric(truth))) #+ xlim(c(25,65)) + xlab(NULL)
-
-
-
-
-out.data$calibration <- ifelse(out.data$dates < "2016-12-31", T, F)
-
-
-fungi <- out.data %>% filter(taxon_name %in% c("wood_saprotroph", "plant_pathogen", "animal_pathogen","litter_saprotroph", "soil_saprotroph", "litter_saprotroph", "ectomycorrhizal") & scenario == "full_uncertainty")
-ggplot(fungi[fungi$plotID=="CPER_001",]) +
-	
-	facet_grid(rows=vars(taxon_name), drop=T, scales="free"#, space="free"
-	) +
-	geom_line(aes(x = dates, y = `med`), show.legend = F) + 
-	geom_ribbon(aes(x = dates, ymin = `lo`, ymax = `hi`, fill = taxon_name), 
-							#fill = "darkblue",
-							alpha=0.4, show.legend = F) +
-	#theme_ipsum(base_size = 18, strip_text_size = 22) + 
-	ggtitle(paste0("Functional group hindcasts at plot CPER_001")) + #scale_x_date() +	
-	#theme_minimal(base_size=20) +
-	#scale_fill_brewer(palette = "Paired") + 
-	theme(panel.spacing = unit(.2, "cm"),
-				plot.margin = unit(c(.2, .2, .2, .2), "cm")) +#+ ylim(c(1.5,7)) +
-	ylab("Abundance") +  
-	geom_point(aes(x = dates, y = as.numeric(truth))) + 
-	scale_shape_manual(values = c(16, 21), name = NULL, labels = c("Calibration","Validation")) 
