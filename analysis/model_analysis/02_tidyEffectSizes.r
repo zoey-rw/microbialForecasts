f40326474353cb0aaabbf4b7490796a8cceced1b
zoey-rw <zoeywerbin@gmail.com>
2022-10-03 14:54:16 -0400

    organizing model analysis scripts

--- analysis/model_analysis/02_tidyEffectSizes.r
+++ analysis/model_analysis/02_tidyEffectSizes.r
@@ -1,30 +1,47 @@
-# Combine effect size estimates (beta covariates) from all model
+# Combine effect size estimates (beta covariates) from all workflows
+# This script must be run before other analysis scripts
+
 source("/projectnb2/talbot-lab-data/zrwerbin/temporal_forecast/source.R")
-pacman::p_load(stringr, forestplot, gridExtra) 
+pacman::p_load(stringr, forestplot, gridExtra)
 
-# Read in summaries
-sum.all.fg <- readRDS("./data/summary/fg_summaries.rds")
-sum.fg <- sum.all.fg$full_uncertainty$summary_df %>% mutate(rank = "functional_group")
+# Read in summaries and combine into fewer dfs for parameter effects
 
-sum.all.tax <- readRDS("./data/summary/taxa_summaries.rds")
-sum.tax <- sum.all.tax$summary_df %>% filter(taxon != "other")
+# Functional groups
+sum.all.fg <- readRDS(here("data", paste0("summary/beta_fg_summaries_20151101_20180101.rds")))
+sum.fg <- sum.all.fg$summary_df %>% mutate(rank = "functional_group")
 
-sum.div.all <- readRDS("./data/summary/div_summaries.rds")
-sum.div <- sum.div.all$summary_df %>% 
-	mutate(rank = paste0("diversity_", group))
+sum.all.fg_refit <- readRDS(here("data", paste0("summary/beta_fg_summaries_20151101_20200101.rds")))
+sum.fg_refit <- sum.all.fg_refit$summary_df %>% mutate(rank = "functional_group")
 
-df <- data.table::rbindlist(list(sum.tax, 
-																 sum.fg, 
-																 sum.div), fill=TRUE)
+# Taxonomic groups
+sum.all.tax <- readRDS(here("data/summary/single_taxon_summaries_201511_201801.rds"))
+sum.tax <- sum.all.tax$summary_df %>% mutate(rank_taxon = rank, rank = rank_name)
+
+sum.all.tax_refit <- readRDS(here("data/summary/single_taxon_summaries_201511_202001.rds"))
+sum.tax_refit <- sum.all.tax_refit$summary_df  %>% mutate(rank_taxon = rank, rank = rank_name)
+
+# Diversity models
+sum.div.all <- readRDS(here("data", "/summary/div_summaries_effects.rds"))
+sum.div <- sum.div.all %>%
+	filter(time_period %in% c("2015-11_2018-01","2015-11_2020-01"))
+sum.div$group <- sapply(str_split(sum.div$taxon, "_",  n = 2), `[`, 2)
+sum.div$rank <- paste0(sum.div$rank, "_", sum.div$group)
 
 
-# Add prettier data values 
-df$pretty_group <- ifelse(df$group=="16S", "Bacteria", "Fungi")
-df$pretty_name <- recode(df$rank, !!!pretty_rank_names)
+df <- data.table::rbindlist(list(sum.tax,
+																 sum.tax_refit,
+																 sum.fg,
+																 sum.fg_refit,
+																 sum.div), fill=TRUE) %>%
+	mutate(pretty_group = ifelse(group == "16S", "Bacteria", "Fungi"))
+
+
+# Add prettier data values
+df$pretty_name <- recode(df$rank, !!!microbialForecast:::pretty_rank_names)
 df$only_rank <- sapply(str_split(df$rank, "_",  n = 2), `[`, 1)
 df$only_rank <- ordered(df$only_rank, levels = c("genus",
 																																			 "family",
-																																			 "order", 
+																																			 "order",
 																																			 "class",
 																																			 "phylum", "functional", "diversity"))
 df$rank <- ordered(df$rank, levels = c("genus_bac","genus_fun",
@@ -35,25 +52,25 @@ df$rank <- ordered(df$rank, levels = c("genus_bac","genus_fun",
 																														 "functional_group", "diversity_16S", "diversity_ITS"))
 df$pretty_name <- ordered(df$pretty_name, levels = c("Genus",
 																																					 "Family",
-																																					 "Order", 
+																																					 "Order",
 																																					 "Class",
 																																					 "Phylum", "Functional group", "Diversity"))
 
 
-# Filter by effect type
+# For saving: filter by effect type
 
 # Linear model beta (covariate) effects
-beta_effects <- df %>% filter(grepl("beta", rowname)) 
-beta_effects$beta <- ordered(beta_effects$beta, levels = c("sin", "cos", 
+beta_effects <- df %>% filter(grepl("beta", rowname))
+beta_effects$beta <- ordered(beta_effects$beta, levels = c("sin", "cos",
 																			 "Ectomycorrhizal trees",
 																			 "LAI",
-																			 "pC", 
+																			 "pC",
 																			 "pH",
-																			 "Temperature", 
+																			 "Temperature",
 																			 "Moisture","rho"))
 levels(beta_effects$beta)[levels(beta_effects$beta)=="Ectomycorrhizal trees"] <- "Ectomycorrhizal\ntrees"
-saveRDS(beta_effects, "./data/summary/all_fcast_effects.rds")
+saveRDS(beta_effects, here("data", "summary/all_fcast_effects.rds"))
 
 # Site effects
-site_effects <- df %>% filter(grepl("site", rowname)) 
-saveRDS(site_effects, "./data/summary/site_effects.rds")
+site_effects <- df %>% filter(grepl("site", rowname))
+saveRDS(site_effects, here("data", "summary/site_effects.rds"))
