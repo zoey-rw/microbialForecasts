1467836b50ffc76223f5831706319baa2c7f10b6
zoey-rw <zoeywerbin@gmail.com>
2022-10-03 13:21:01 -0400

    add new workflow structure

--- analysis/workflows/a_taxonomy/02_summary_convergence.r
+++ analysis/workflows/a_taxonomy/02_summary_convergence.r
@@ -1,137 +1,85 @@
+# Summarize MCMC output from calibration, cyclical-covariate single-taxon models
+# To determine which should be included in dirichlet models
 
 source("/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/source.R")
 
-	file.list <- list.files("/projectnb2/talbot-lab-data/zrwerbin/temporal_forecast/data/model_outputs/convergence_testing", pattern = "20151101_20180101", full.names = T)
-	f <- "/projectnb2/talbot-lab-data/zrwerbin/temporal_forecast/data/model_outputs/convergence_testing/samples_class_bac_acidimicrobiia_20151101_20180101.rds"
-	
-	
-	testing <- summarize_tax_model(f)
-	file_summaries <- lapply(file.list[1:2], summarize_tax_model)
-	
+file.list = intersect(list.files(here("data/model_outputs/single_taxon/cycl_only"),recursive = T,
+																	pattern = "20151101_20180101", full.names = T),
+											 list.files(here("data/model_outputs/single_taxon/cycl_only"), recursive = T,
+											 					 pattern = "samples", full.names = T))
+#	out <- summarize_tax_model(file.list[[340]], save_summary=T, drop_other = T)
+
 	cl <- makeCluster(28, outfile="")
 	registerDoParallel(cl)
 	#Run for multiple chains, in parallel (via PSOCK)
-	file_summaries = foreach(f=file.list,
-													 .errorhandling = 'pass') %dopar% {
-													 	source("/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/source.R")
-													 	
-													 	require(stringr)
-													 	out <- summarize_tax_model(f)
+	file_summaries = foreach(f=file.list, .errorhandling = "pass") %dopar% {
+		source("/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/source.R")
+													 	out <- summarize_tax_model(f, save_summary=T, drop_other = F)
 													 	return(out)
 													 }
 	stopCluster(cl)
-	
-	
-	summary_df <- map(file_summaries, 1) %>% plyr::rbind.fill() %>% 
-		mutate(time_period = recode(as.character(time_period), !!!date_recode))
-	plot_est <- map(file_summaries, 3) %>% plyr::rbind.fill() %>% 
-		mutate(time_period = recode(as.character(time_period), !!!date_recode))
-	gelman_list <- map(file_summaries, 4) 
-	names(gelman_list) <- paste(basename(dirname(file.list)),basename(file.list), sep = "_")
-	scores.list <- map(file_summaries, 2) %>% plyr::rbind.fill() %>% 
-		mutate(time_period = recode(as.character(time_period), !!!date_recode))
-	
-	
+
+
+
+	file.list2 = intersect(list.files(here("data/model_outputs/single_taxon/cycl_only"),
+																		pattern = "20151101_20180101", full.names = T),
+												 list.files(here("data/model_outputs/single_taxon/cycl_only"),
+												 					 pattern = "summary", full.names = T))
+
+	file_summaries <- purrr::map(file.list2, readRDS)
+
+	#in1 <- readRDS(file.list2[[1]])
+
+
+	summary_df <- map_df(file_summaries, 1) %>%
+		mutate(time_period = recode(as.character(time_period), !!!microbialForecast:::date_recode))
+	plot_est <- map_df(file_summaries, 2) %>%
+		mutate(time_period = recode(as.character(time_period), !!!microbialForecast:::date_recode))
+	gelman_list <- map_df(file_summaries, 3)
+
+
 	saveRDS(list(summary_df = summary_df,
 							 plot_est = plot_est,
-							 scores.list = scores.list,
-							 gelman.summary = gelman_list), 
+							 gelman.summary = gelman_list),
 					"/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/data/summary/taxon_convergence_summaries_201511_201801.rds")
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	# old
-	
-	plot_est_df_all_ranks <- plyr::rbind.fill(plot_est_df_all)
-	scores.list_all_ranks <- plyr::rbind.fill(allplots.scores.list)
-	summary_df_all_ranks <- do.call(rbind, summary_df_all)
-	
-	
-	summary_df_all_ranks <- summary_df_all_ranks %>% mutate(fcast_type = "Taxonomic_convergence",
-																	fcast_period = "calibration",
-																	fg_cat = "Bacterial taxa",
-																	group = "16S") 
-	summary_df_all_ranks[grepl("fun$", summary_df_all_ranks$rank.name),]$fg_cat = "Fungal taxa"
-	summary_df_all_ranks[grepl("fun$", summary_df_all_ranks$rank.name),]$group = "ITS"
-
-	gelman.summary <- do.call(rbind, gelman_list)
-	rownames(gelman.summary) <- NULL
-	
-	
-saveRDS(list(summary_df = summary_df_all_ranks,
-						 gelman.summary = gelman.summary), "/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/data/summary/allrank_convergence_summaries.rds")
 
 
 
-gelman_list2 <- list()
-for (f in file.list){
-	
-	# Prep model inputs/outputs.
-	model.path <- f 
-	
-	split <- str_split(basename(f), "_|\\.")
-	rank_only <- split[[1]][2]
-	kingdom <- split[[1]][3]
-	rank.name <- paste0(rank_only, "_", kingdom)
-	taxon.name <- split[[1]][4]
-	
-	cat(paste("Summarizing convergence output of ", rank.name, ", group: ", taxon.name))
-	
-	# Read in samples
-	read_in <- readRDS(model.path)
-	samples <- read_in$samples
-	
-	## Calculate gelman diagnostics to assess convergence
-	gd.orig <- gelman.diag(samples)
-	gd <- cbind.data.frame(gd.orig[[1]], effSize = effectiveSize(samples))
-	gd$rank.name <- rank.name
-	gd$taxon.name <- taxon.name
-	gd$niter <- read_in$metadata[[2]]
-	gd$nburnin <- read_in$metadata[[3]]
-	
-	gd2 <- cbind.data.frame(gd.orig[[2]])
-	gd2$rank.name <- rank.name
-	gd2$taxon.name <- taxon.name
-	gd2$niter <- read_in$metadata[[2]]
-	gd2$nburnin <- read_in$metadata[[3]]
-	gelman_list2[[f]] <- gd2
-}
-	
-gelman.summary2 <- do.call(rbind, gelman_list2)
+	convergence_in <- readRDS("/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/data/summary/taxon_convergence_summaries_201511_201801.rds")
+
+	scores.list <- convergence_in$scores.list
 
 
+gelman.summary <- convergence_in$gelman.summary
 
-gelman.summary_remove <- gelman.summary[gelman.summary$`Upper C.I.` > 20,]
+gelman.summary <- gelman.summary %>%
+	separate(rank.name, sep = "_", into = c("only_rank", "group", NA), remove = F) %>%
+	separate(rank.name, sep = "_bac_|_fun_", into = c(NA, "taxon.name"), remove = F) %>%
+	mutate(rank_group = paste(only_rank, group, sep = "_"))
+
+by.group <- gelman.summary %>%
+	group_by(group, rank_group, only_rank, rank.name, taxon.name) %>%
+	dplyr::summarize(median_gbr = median(`Point est.`))
+
+ggplot(by.group) + geom_jitter(aes(x = only_rank, y = median_gbr, color = group)) + ylim(c(0,10))
 
 
-
-df <- readRDS("/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/data/summary/allrank_convergence_summaries.rds")
-
-gelman.summary <- df$gelman.summary
-summary.df <- df$summary_df
 
 keep_taxa <- list()
 
-
-by.rank <- gelman.summary %>% 
-	group_by(rank.name, taxon.name) %>% 
+by.rank <- gelman.summary %>%
+	group_by(rank_group, taxon.name) %>%
 	summarize(median_gbr = median(`Point est.`))
 
-remove <- by.rank[by.rank$median_gbr > 2.7,]$taxon.name
-keep <- by.rank[by.rank$median_gbr <= 2.2,]$taxon.name
-keep <- by.rank[by.rank$median_gbr <= 2.2,]
+keep <- by.group[by.group$median_gbr <= 2,]
+keep_list <- split(keep, keep$rank_group)
 
+rerun <- by.group[by.group$median_gbr > 1.5,]
+rerun_list <- split(rerun, rerun$rank_group)
+gelman.summary_remove <- gelman.summary[gelman.summary$taxon.name %in% rerun$taxon.name,]
 
-keep_list <- split(keep, keep$rank.name)
 #by.rank[by.rank$taxon.name %in% keep,]
 
+# This was actually run with cycl_only models
 saveRDS(keep_list, "/projectnb/talbot-lab-data/zrwerbin/temporal_forecast/data/summary/converged_taxa_list.rds")
-
-gelman.summary_remove <- gelman.summary[gelman.summary$taxon.name %in% remove,]
 
--- analysis/workflows/c_diversity/02_summary_diversity.r
+++ analysis/workflows/c_diversity/02_summary_diversity.r
@@ -1,5 +1,5 @@
 # Summarize outputs from diversity models
-pacman::p_load(coda, tidyverse) 
+pacman::p_load(coda, tidyverse)
 source("./source.R")
 
 plot_est_df_all <- list()
@@ -7,7 +7,7 @@ summary_df_all <- list()
 gelman_list <- list()
 allplots.scores.list <- list()
 
-file.list <- list.files(path = "/projectnb2/talbot-lab-data/zrwerbin/temporal_forecast/data/model_outputs/diversity/", 
+file.list <- list.files(path = "/projectnb2/talbot-lab-data/zrwerbin/temporal_forecast/data/model_outputs/diversity/",
 												recursive = T,
 												pattern = "samples_div_[I1]",
 												full.names = T)
@@ -15,13 +15,13 @@ file.list <- list.files(path = "/projectnb2/talbot-lab-data/zrwerbin/temporal_fo
 file_path = file.list[[1]]
 file_summaries <- lapply(file.list, summarize_fg_div_model)
 
-summary_df <- map(file_summaries, 1) %>% plyr::rbind.fill() %>% 
+summary_df <- map(file_summaries, 1) %>% plyr::rbind.fill() %>%
 	mutate(time_period = recode(as.character(time_period), !!!date_recode))
-plot_est <- map(file_summaries, 3) %>% plyr::rbind.fill() %>% 
+plot_est <- map(file_summaries, 3) %>% plyr::rbind.fill() %>%
 	mutate(time_period = recode(as.character(time_period), !!!date_recode))
-gelman_list <- map(file_summaries, 4) 
+gelman_list <- map(file_summaries, 4)
 names(gelman_list) <- paste(basename(dirname(file.list)),basename(file.list), sep = "_")
-scores.list <- map(file_summaries, 2) %>% plyr::rbind.fill() %>% 
+scores.list <- map(file_summaries, 2) %>% plyr::rbind.fill() %>%
 	mutate(time_period = recode(as.character(time_period), !!!date_recode))
 
 
@@ -52,6 +52,9 @@ combined[combined$time_period=="2015-11_2018-01",]$time_period <- "2015-11_2020-
 combined[combined$time_period=="refit",]$time_period				     <- "2013-06_2017-01"
 combined[combined$time_period=="calibration",]$time_period       <- "2013-06_2016-01"
 
+
+saveRDS(combined, here("data", "/summary/div_summaries_effects.rds"))
+
 # ## GGPLOT
 
 cycl_combined <- combined %>% filter(model_name == "cycl_only")
@@ -59,12 +62,12 @@ cycl_combined <- combined %>% filter(model_name == "cycl_only")
 ##### Effect sizes & directions for refit vs calibration ####
 ggplot(data=beta_recent,
 			 aes(x = reorder(beta, Mean),y = Mean)) +
-	facet_grid(cols = vars(model_name), 
+	facet_grid(cols = vars(model_name),
 						 rows = vars(group), drop = T) +
-	geom_point(aes(shape = as.factor(time_period), 
+	geom_point(aes(shape = as.factor(time_period),
 								 color = beta), size = 4) +
-	labs(col = "Parameter", title = "Absolute effect size") + 
-	xlab("Parameter")+ 
+	labs(col = "Parameter", title = "Absolute effect size") +
+	xlab("Parameter")+
 	ylab(NULL) +
 	theme_bw() + theme(#axis.ticks.x=element_blank(),
 		text = element_text(size = 16),
@@ -72,18 +75,18 @@ ggplot(data=beta_recent,
 			angle = 320, vjust=1, hjust = -0.05),
 		axis.title=element_text(size=22,face="bold")
 		#strip.text.y = element_text(size=24,hjust=0,vjust = 1,angle=180,face="bold")
-	) + #scale_shape_manual(values = c(21, 16), name = NULL, 
-			#									 labels = c("Calibration","Full dataset")) + 
+	) + #scale_shape_manual(values = c(21, 16), name = NULL,
+			#									 labels = c("Calibration","Full dataset")) +
 	geom_hline(yintercept = 0)
 
 ggplot(data=combined,
 			 aes(x = reorder(beta, Mean),y = Mean)) +
-	facet_grid(cols = vars(model_name), 
+	facet_grid(cols = vars(model_name),
 						 rows = vars(group), drop = T) +
-	geom_point(aes(shape = as.factor(time_period), 
+	geom_point(aes(shape = as.factor(time_period),
 								 color = as.factor(time_period)), size = 4) +
-	labs(col = "Parameter", title = "Calibration period effect size") + 
-	xlab("Parameter")+ 
+	labs(col = "Parameter", title = "Calibration period effect size") +
+	xlab("Parameter")+
 	ylab(NULL) +
 	theme_bw() + theme(#axis.ticks.x=element_blank(),
 		text = element_text(size = 16),
@@ -91,12 +94,12 @@ ggplot(data=combined,
 			angle = 320, vjust=1, hjust = -0.05),
 		axis.title=element_text(size=22,face="bold")
 		#strip.text.y = element_text(size=24,hjust=0,vjust = 1,angle=180,face="bold")
-	) + geom_hline(yintercept = 0) + 
+	) + geom_hline(yintercept = 0) +
 	scale_colour_manual(name = "Time period",
 										labels = c("2015-11_2018-01",
 															 "2016-01_2020-01",
 															 "2013-06_2017-01",
-															 "2013-06_2016-01"), values = c(1,2,3,4)) +   
+															 "2013-06_2016-01"), values = c(1,2,3,4)) +
 	scale_shape_manual(name = "Time period",
 										 labels = c("2015-11_2018-01",
 										 					 "2016-01_2020-01",
@@ -108,12 +111,12 @@ ggplot(data=combined,
 # Abs. effect sizes for all covariates vs cyc alone
 ggplot(data=refit,
 			 aes(x = reorder(beta, effSize),y = effSize)) +
-	facet_grid(#cols = vars(model_name), 
+	facet_grid(#cols = vars(model_name),
 						 rows = vars(group), drop = T) +
-	geom_point(aes(shape = as.factor(model_name), 
+	geom_point(aes(shape = as.factor(model_name),
 								 color = beta), size = 4) +
-	labs(col = "Parameter", title = "Absolute effect size") + 
-	xlab("Parameter")+ 
+	labs(col = "Parameter", title = "Absolute effect size") +
+	xlab("Parameter")+
 	ylab(NULL) +
 	theme_bw() + theme(#axis.ticks.x=element_blank(),
 		text = element_text(size = 16),
@@ -121,8 +124,8 @@ ggplot(data=refit,
 			angle = 320, vjust=1, hjust = -0.05),
 		axis.title=element_text(size=22,face="bold")
 		#strip.text.y = element_text(size=24,hjust=0,vjust = 1,angle=180,face="bold")
-	) + scale_shape_manual(values = c(21, 16), name = NULL, 
-												 labels = c("All covariates","No environmental covariates")) 
+	) + scale_shape_manual(values = c(21, 16), name = NULL,
+												 labels = c("All covariates","No environmental covariates"))
 
 
 ## Violin plots of parameter estimates
@@ -130,8 +133,8 @@ samps <- read_in$sample.list$full_uncertainty_ITS
 samps <- do.call(rbind.data.frame, samps[,grep("beta|rho", colnames(samps[[1]]))])
 samps_long <- samps %>% rownames_to_column("rowname") %>% pivot_longer(2:8)
 
-samps_long <- samps_long %>% 
-	mutate(beta_num = gsub("beta\\[|\\]", "", name)) %>% 
+samps_long <- samps_long %>%
+	mutate(beta_num = gsub("beta\\[|\\]", "", name)) %>%
 	mutate(beta = recode(beta_num,
 											 "1" = "Temperature",
 											 "2" = "Moisture",
@@ -142,24 +145,24 @@ samps_long <- samps_long %>%
 											 .missing = "Autocorrelation"))
 ggplot(data=samps_long,
 			 aes(x = reorder(beta, value),y = value)) +
-	geom_violin(aes(fill = beta), trim=FALSE) + 
-	ylab("Effect size") + 
-	xlab("Parameter")+ ggtitle("Drivers of Shannon evenness of soil fungi") + theme_minimal(base_size = 16) + geom_hline(aes(yintercept=0), linetype=2) + 
+	geom_violin(aes(fill = beta), trim=FALSE) +
+	ylab("Effect size") +
+	xlab("Parameter")+ ggtitle("Drivers of Shannon evenness of soil fungi") + theme_minimal(base_size = 16) + geom_hline(aes(yintercept=0), linetype=2) +
 	theme(
 		axis.text.x=element_text(#angle = 45, hjust = 1, vjust = 1),
 			angle = 320, vjust=1, hjust = -0.05),
 		axis.title=element_text(size=18,face="bold")
-	) 
+	)
 
 
 # Takes a long time to render...
 ggplot(data=samps_long,
 			 aes(x = reorder(beta, value),y = value)) +
-	geom_jitter(aes(color = beta), width = .2, height = 0, alpha = .3) + 
-	ylab("Effect size") + 
-	xlab("Parameter")+ ggtitle("Drivers of Shannon evenness of soil fungi") + theme_minimal(base_size = 16) + geom_hline(aes(yintercept=0), linetype=2) + 
+	geom_jitter(aes(color = beta), width = .2, height = 0, alpha = .3) +
+	ylab("Effect size") +
+	xlab("Parameter")+ ggtitle("Drivers of Shannon evenness of soil fungi") + theme_minimal(base_size = 16) + geom_hline(aes(yintercept=0), linetype=2) +
 	theme(
 		axis.text.x=element_text(#angle = 45, hjust = 1, vjust = 1),
 			angle = 320, vjust=1, hjust = -0.05),
 		axis.title=element_text(size=18,face="bold")
-	) 
+	)
